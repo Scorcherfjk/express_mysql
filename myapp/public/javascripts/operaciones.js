6f6efb8f30b34829e8892880a89c695e99948442
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-11-05 17:20:27 -0500

    operaciones matematicas automaticas

---
+++ myapp/public/javascripts/operaciones.js
@@ -0,0 +1,137 @@
+$(() => {
+    /** D11 */
+    /** 1 */
+    $("#d11_6_1").keyup(function(){ var d11_6_1 = $(this).val(); var d11_7_1= $("#d11_7_1").val(); $("#d11_8_1").val(d11_7_1*d11_6_1); })
+    $("#d11_7_1").keyup(function(){ var d11_7_1= $(this).val(); var d11_6_1 = $("#d11_6_1").val(); $("#d11_8_1").val(d11_6_1*d11_7_1); })
+    /** 2 */
+    $("#d11_6_2").keyup(function(){ var d11_6_2 = $(this).val(); var d11_7_2= $("#d11_7_2").val(); $("#d11_8_2").val(d11_7_2*d11_6_2); })
+    $("#d11_7_2").keyup(function(){ var d11_7_2= $(this).val(); var d11_6_2 = $("#d11_6_2").val(); $("#d11_8_2").val(d11_6_2*d11_7_2); })
+    /** 3 */
+    $("#d11_6_3").keyup(function(){ var d11_6_3 = $(this).val(); var d11_7_3= $("#d11_7_3").val(); $("#d11_8_3").val(d11_7_3*d11_6_3); })
+    $("#d11_7_3").keyup(function(){ var d11_7_3= $(this).val(); var d11_6_3 = $("#d11_6_3").val(); $("#d11_8_3").val(d11_6_3*d11_7_3); })
+    /** 4 */
+    $("#d11_6_4").keyup(function(){ var d11_6_4 = $(this).val(); var d11_7_4= $("#d11_7_4").val(); $("#d11_8_4").val(d11_7_4*d11_6_4); })
+    $("#d11_7_4").keyup(function(){ var d11_7_4= $(this).val(); var d11_6_4 = $("#d11_6_4").val(); $("#d11_8_4").val(d11_6_4*d11_7_4); })
+    /** 5 */
+    $("#d11_6_5").keyup(function(){ var d11_6_5 = $(this).val(); var d11_7_5= $("#d11_7_5").val(); $("#d11_8_5").val(d11_7_5*d11_6_5); })
+    $("#d11_7_5").keyup(function(){ var d11_7_5= $(this).val(); var d11_6_5 = $("#d11_6_5").val(); $("#d11_8_5").val(d11_6_5*d11_7_5); })
+
+    /** D12 */
+    /** 1 */
+    $("#d12_4_1").keyup(function(){ var d12_4_1 = $(this).val(); var d12_5_1= $("#d12_5_1").val(); $("#d12_6_1").val(d12_5_1*d12_4_1); })
+    $("#d12_5_1").keyup(function(){ var d12_5_1= $(this).val(); var d12_4_1 = $("#d12_4_1").val(); $("#d12_6_1").val(d12_4_1*d12_5_1); })
+    /** 2 */
+    $("#d12_4_2").keyup(function(){ var d12_4_2 = $(this).val(); var d12_5_2= $("#d12_5_2").val(); $("#d12_6_2").val(d12_5_2*d12_4_2); })
+    $("#d12_5_2").keyup(function(){ var d12_5_2= $(this).val(); var d12_4_2 = $("#d12_4_2").val(); $("#d12_6_2").val(d12_4_2*d12_5_2); })
+    /** 3 */
+    $("#d12_4_3").keyup(function(){ var d12_4_3 = $(this).val(); var d12_5_3= $("#d12_5_3").val(); $("#d12_6_3").val(d12_5_3*d12_4_3); })
+    $("#d12_5_3").keyup(function(){ var d12_5_3= $(this).val(); var d12_4_3 = $("#d12_4_3").val(); $("#d12_6_3").val(d12_4_3*d12_5_3); })
+    /** 4 */
+    $("#d12_4_4").keyup(function(){ var d12_4_4 = $(this).val(); var d12_5_4= $("#d12_5_4").val(); $("#d12_6_4").val(d12_5_4*d12_4_4); })
+    $("#d12_5_4").keyup(function(){ var d12_5_4= $(this).val(); var d12_4_4 = $("#d12_4_4").val(); $("#d12_6_4").val(d12_4_4*d12_5_4); })
+    /** 5 */
+    $("#d12_4_5").keyup(function(){ var d12_4_5 = $(this).val(); var d12_5_5= $("#d12_5_5").val(); $("#d12_6_5").val(d12_5_5*d12_4_5); })
+    $("#d12_5_5").keyup(function(){ var d12_5_5= $(this).val(); var d12_4_5 = $("#d12_4_5").val(); $("#d12_6_5").val(d12_4_5*d12_5_5); })
+
+    /** D13 */
+    /** 1 */
+    $("#d13_3_1").keyup(function(){ var d13_3_1 = $(this).val(); var d13_4_1= $("#d13_4_1").val(); $("#d13_5_1").val(d13_4_1*d13_3_1); })
+    $("#d13_4_1").keyup(function(){ var d13_4_1= $(this).val(); var d13_3_1 = $("#d13_3_1").val(); $("#d13_5_1").val(d13_3_1*d13_4_1); })
+    /** 2 */
+    $("#d13_3_2").keyup(function(){ var d13_3_2 = $(this).val(); var d13_4_2= $("#d13_4_2").val(); $("#d13_5_2").val(d13_4_2*d13_3_2); })
+    $("#d13_4_2").keyup(function(){ var d13_4_2= $(this).val(); var d13_3_2 = $("#d13_3_2").val(); $("#d13_5_2").val(d13_3_2*d13_4_2); })
+    /** 3 */
+    $("#d13_3_3").keyup(function(){ var d13_3_3 = $(this).val(); var d13_4_3= $("#d13_4_3").val(); $("#d13_5_3").val(d13_4_3*d13_3_3); })
+    $("#d13_4_3").keyup(function(){ var d13_4_3= $(this).val(); var d13_3_3 = $("#d13_3_3").val(); $("#d13_5_3").val(d13_3_3*d13_4_3); })
+    /** 4 */
+    $("#d13_3_4").keyup(function(){ var d13_3_4 = $(this).val(); var d13_4_4= $("#d13_4_4").val(); $("#d13_5_4").val(d13_4_4*d13_3_4); })
+    $("#d13_4_4").keyup(function(){ var d13_4_4= $(this).val(); var d13_3_4 = $("#d13_3_4").val(); $("#d13_5_4").val(d13_3_4*d13_4_4); })
+    /** 5 */
+    $("#d13_3_5").keyup(function(){ var d13_3_5 = $(this).val(); var d13_4_5= $("#d13_4_5").val(); $("#d13_5_5").val(d13_4_5*d13_3_5); })
+    $("#d13_4_5").keyup(function(){ var d13_4_5= $(this).val(); var d13_3_5 = $("#d13_3_5").val(); $("#d13_5_5").val(d13_3_5*d13_4_5); })
+
+    /** D14 */
+    /** 1 */
+    $("#d14_3_1").keyup(function(){ var d14_3_1 = $(this).val(); var d14_4_1= $("#d14_4_1").val(); $("#d14_5_1").val(d14_4_1*d14_3_1); })
+    $("#d14_4_1").keyup(function(){ var d14_4_1= $(this).val(); var d14_3_1 = $("#d14_3_1").val(); $("#d14_5_1").val(d14_3_1*d14_4_1); })
+    /** 2 */
+    $("#d14_3_2").keyup(function(){ var d14_3_2 = $(this).val(); var d14_4_2= $("#d14_4_2").val(); $("#d14_5_2").val(d14_4_2*d14_3_2); })
+    $("#d14_4_2").keyup(function(){ var d14_4_2= $(this).val(); var d14_3_2 = $("#d14_3_2").val(); $("#d14_5_2").val(d14_3_2*d14_4_2); })
+    /** 3 */
+    $("#d14_3_3").keyup(function(){ var d14_3_3 = $(this).val(); var d14_4_3= $("#d14_4_3").val(); $("#d14_5_3").val(d14_4_3*d14_3_3); })
+    $("#d14_4_3").keyup(function(){ var d14_4_3= $(this).val(); var d14_3_3 = $("#d14_3_3").val(); $("#d14_5_3").val(d14_3_3*d14_4_3); })
+    /** 4 */
+    $("#d14_3_4").keyup(function(){ var d14_3_4 = $(this).val(); var d14_4_4= $("#d14_4_4").val(); $("#d14_5_4").val(d14_4_4*d14_3_4); })
+    $("#d14_4_4").keyup(function(){ var d14_4_4= $(this).val(); var d14_3_4 = $("#d14_3_4").val(); $("#d14_5_4").val(d14_3_4*d14_4_4); })
+    /** 5 */
+    $("#d14_3_5").keyup(function(){ var d14_3_5 = $(this).val(); var d14_4_5= $("#d14_4_5").val(); $("#d14_5_5").val(d14_4_5*d14_3_5); })
+    $("#d14_4_5").keyup(function(){ var d14_4_5= $(this).val(); var d14_3_5 = $("#d14_3_5").val(); $("#d14_5_5").val(d14_3_5*d14_4_5); })
+
+    /** D15 */
+    /** 1 */
+    $("#d15_7_1").keyup(function(){ var d15_7_1 = $(this).val(); var d15_8_1= $("#d15_8_1").val(); $("#d15_9_1").val(d15_8_1*d15_7_1); })
+    $("#d15_8_1").keyup(function(){ var d15_8_1= $(this).val(); var d15_7_1 = $("#d15_7_1").val(); $("#d15_9_1").val(d15_7_1*d15_8_1); })
+    /** 2 */
+    $("#d15_7_2").keyup(function(){ var d15_7_2 = $(this).val(); var d15_8_2= $("#d15_8_2").val(); $("#d15_9_2").val(d15_8_2*d15_7_2); })
+    $("#d15_8_2").keyup(function(){ var d15_8_2= $(this).val(); var d15_7_2 = $("#d15_7_2").val(); $("#d15_9_2").val(d15_7_2*d15_8_2); })
+    /** 3 */
+    $("#d15_7_3").keyup(function(){ var d15_7_3 = $(this).val(); var d15_8_3= $("#d15_8_3").val(); $("#d15_9_3").val(d15_8_3*d15_7_3); })
+    $("#d15_8_3").keyup(function(){ var d15_8_3= $(this).val(); var d15_7_3 = $("#d15_7_3").val(); $("#d15_9_3").val(d15_7_3*d15_8_3); })
+    /** 4 */
+    $("#d15_7_4").keyup(function(){ var d15_7_4 = $(this).val(); var d15_8_4= $("#d15_8_4").val(); $("#d15_9_4").val(d15_8_4*d15_7_4); })
+    $("#d15_8_4").keyup(function(){ var d15_8_4= $(this).val(); var d15_7_4 = $("#d15_7_4").val(); $("#d15_9_4").val(d15_7_4*d15_8_4); })
+    /** 5 */
+    $("#d15_7_5").keyup(function(){ var d15_7_5 = $(this).val(); var d15_8_5= $("#d15_8_5").val(); $("#d15_9_5").val(d15_8_5*d15_7_5); })
+    $("#d15_8_5").keyup(function(){ var d15_8_5= $(this).val(); var d15_7_5 = $("#d15_7_5").val(); $("#d15_9_5").val(d15_7_5*d15_8_5); })
+
+    /** D16 */
+    /** 1 */
+    $("#d16_6_1").keyup(function(){ var d16_6_1 = $(this).val(); var d16_7_1= $("#d16_7_1").val(); $("#d16_8_1").val(d16_7_1*d16_6_1); })
+    $("#d16_7_1").keyup(function(){ var d16_7_1= $(this).val(); var d16_6_1 = $("#d16_6_1").val(); $("#d16_8_1").val(d16_6_1*d16_7_1); })
+    /** 2 */
+    $("#d16_6_2").keyup(function(){ var d16_6_2 = $(this).val(); var d16_7_2= $("#d16_7_2").val(); $("#d16_8_2").val(d16_7_2*d16_6_2); })
+    $("#d16_7_2").keyup(function(){ var d16_7_2= $(this).val(); var d16_6_2 = $("#d16_6_2").val(); $("#d16_8_2").val(d16_6_2*d16_7_2); })
+    /** 3 */
+    $("#d16_6_3").keyup(function(){ var d16_6_3 = $(this).val(); var d16_7_3= $("#d16_7_3").val(); $("#d16_8_3").val(d16_7_3*d16_6_3); })
+    $("#d16_7_3").keyup(function(){ var d16_7_3= $(this).val(); var d16_6_3 = $("#d16_6_3").val(); $("#d16_8_3").val(d16_6_3*d16_7_3); })
+    /** 4 */
+    $("#d16_6_4").keyup(function(){ var d16_6_4 = $(this).val(); var d16_7_4= $("#d16_7_4").val(); $("#d16_8_4").val(d16_7_4*d16_6_4); })
+    $("#d16_7_4").keyup(function(){ var d16_7_4= $(this).val(); var d16_6_4 = $("#d16_6_4").val(); $("#d16_8_4").val(d16_6_4*d16_7_4); })
+    /** 5 */
+    $("#d16_6_5").keyup(function(){ var d16_6_5 = $(this).val(); var d16_7_5= $("#d16_7_5").val(); $("#d16_8_5").val(d16_7_5*d16_6_5); })
+    $("#d16_7_5").keyup(function(){ var d16_7_5= $(this).val(); var d16_6_5 = $("#d16_6_5").val(); $("#d16_8_5").val(d16_6_5*d16_7_5); })
+
+    /** D17 */
+    /** 1 */
+    $("#d17_4_1").keyup(function(){ var d17_4_1 = $(this).val(); var d17_5_1= $("#d17_5_1").val(); $("#d17_6_1").val(d17_5_1*d17_4_1); })
+    $("#d17_5_1").keyup(function(){ var d17_5_1= $(this).val(); var d17_4_1 = $("#d17_4_1").val(); $("#d17_6_1").val(d17_4_1*d17_5_1); })
+    /** 2 */
+    $("#d17_4_2").keyup(function(){ var d17_4_2 = $(this).val(); var d17_5_2= $("#d17_5_2").val(); $("#d17_6_2").val(d17_5_2*d17_4_2); })
+    $("#d17_5_2").keyup(function(){ var d17_5_2= $(this).val(); var d17_4_2 = $("#d17_4_2").val(); $("#d17_6_2").val(d17_4_2*d17_5_2); })
+    /** 3 */
+    $("#d17_4_3").keyup(function(){ var d17_4_3 = $(this).val(); var d17_5_3= $("#d17_5_3").val(); $("#d17_6_3").val(d17_5_3*d17_4_3); })
+    $("#d17_5_3").keyup(function(){ var d17_5_3= $(this).val(); var d17_4_3 = $("#d17_4_3").val(); $("#d17_6_3").val(d17_4_3*d17_5_3); })
+    /** 4 */
+    $("#d17_4_4").keyup(function(){ var d17_4_4 = $(this).val(); var d17_5_4= $("#d17_5_4").val(); $("#d17_6_4").val(d17_5_4*d17_4_4); })
+    $("#d17_5_4").keyup(function(){ var d17_5_4= $(this).val(); var d17_4_4 = $("#d17_4_4").val(); $("#d17_6_4").val(d17_4_4*d17_5_4); })
+    /** 5 */
+    $("#d17_4_5").keyup(function(){ var d17_4_5 = $(this).val(); var d17_5_5= $("#d17_5_5").val(); $("#d17_6_5").val(d17_5_5*d17_4_5); })
+    $("#d17_5_5").keyup(function(){ var d17_5_5= $(this).val(); var d17_4_5 = $("#d17_4_5").val(); $("#d17_6_5").val(d17_4_5*d17_5_5); })
+
+    /** D18 */ 
+    /** 1 */
+    $("#d18_3_1").keyup(function(){ var d18_3_1 = $(this).val(); var d18_4_1= $("#d18_4_1").val(); $("#d18_5_1").val(d18_4_1*d18_3_1); })
+    $("#d18_4_1").keyup(function(){ var d18_4_1= $(this).val(); var d18_3_1 = $("#d18_3_1").val(); $("#d18_5_1").val(d18_3_1*d18_4_1); })
+    /** 2 */
+    $("#d18_3_2").keyup(function(){ var d18_3_2 = $(this).val(); var d18_4_2= $("#d18_4_2").val(); $("#d18_5_2").val(d18_4_2*d18_3_2); })
+    $("#d18_4_2").keyup(function(){ var d18_4_2= $(this).val(); var d18_3_2 = $("#d18_3_2").val(); $("#d18_5_2").val(d18_3_2*d18_4_2); })
+    /** 3 */
+    $("#d18_3_3").keyup(function(){ var d18_3_3 = $(this).val(); var d18_4_3= $("#d18_4_3").val(); $("#d18_5_3").val(d18_4_3*d18_3_3); })
+    $("#d18_4_3").keyup(function(){ var d18_4_3= $(this).val(); var d18_3_3 = $("#d18_3_3").val(); $("#d18_5_3").val(d18_3_3*d18_4_3); })
+    /** 4 */
+    $("#d18_3_4").keyup(function(){ var d18_3_4 = $(this).val(); var d18_4_4= $("#d18_4_4").val(); $("#d18_5_4").val(d18_4_4*d18_3_4); })
+    $("#d18_4_4").keyup(function(){ var d18_4_4= $(this).val(); var d18_3_4 = $("#d18_3_4").val(); $("#d18_5_4").val(d18_3_4*d18_4_4); })
+    /** 5 */
+    $("#d18_3_5").keyup(function(){ var d18_3_5 = $(this).val(); var d18_4_5= $("#d18_4_5").val(); $("#d18_5_5").val(d18_4_5*d18_3_5); })
+    $("#d18_4_5").keyup(function(){ var d18_4_5= $(this).val(); var d18_3_5 = $("#d18_3_5").val(); $("#d18_5_5").val(d18_3_5*d18_4_5); })
+})
